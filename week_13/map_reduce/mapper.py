7b95a8b902b3fbd00d97186c1e8595f325fb15b7
Lex <lexmckenz@gmail.com>
2026-04-08 17:42:26 -0500

    map reduce

---
+++ week_13/map_reduce/mapper.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import sys
+
+def mapper():
+    """
+    Reads in a sentence and maps the values.
+    Mapping the values means it will give a
+    count of 1 to every word in the sentence
+
+    Words are defined if there is a space 
+    between them.
+    """
+
+    #stdin = standard input
+    for line in sys.stdin:
+        #strip white space at the beginning
+        # and end of the line
+        line = line.strip()
+        line = line.replace(",","")
+        line = line.replace(".","")
+
+        #split the line into words
+        words = line.split()
+
+        # process each word and assign
+        # a value of 1 to each word
+        for word in words:
+            print(word + "\t1")
+            sys.stdout.flush() #might need this line to show in console output (windows)
+
+
+if __name__=="__main__":
+    mapper()
